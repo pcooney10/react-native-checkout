53b3beeda2442bee96abc252c060a0459117c26b
pcooney10 <pcooney9@fordham.edu>
2017-09-05 14:55:54 -0400

    fixed typo

--- src/components/selectPayment/index.js
+++ src/components/selectPayment/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import _ from 'lodash'
 import PaymentMethods from '../paymentMethods'
 import defaultStyles from './defaultStyles'
